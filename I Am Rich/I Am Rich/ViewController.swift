6fa7baa21d1c93ee3319b6deb56a2ca391e0306a
Arno Vandijck <vandijckarno@hotmail.com>
2021-10-24 01:15:08 +0200

    Little projects I made for fun

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Arno Vandijck on 28/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
