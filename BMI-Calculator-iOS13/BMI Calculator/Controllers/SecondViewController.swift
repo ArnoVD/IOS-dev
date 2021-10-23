df51d745ecf8280e7652245c94afed22c0947146
Arno Vandijck <vandijckarno@hotmail.com>
2021-10-24 01:19:33 +0200

    Simple BMI calculator

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Arno Vandijck on 21/07/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    <#code#>
+}
